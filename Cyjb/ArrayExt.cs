9d7c64b276e070296e6cdd0c962e7089c33f5617
CYJB <f8cyjb@126.com>
2012-11-23 23:25:54 +0800

    修复了随机排序的统计数据不平均的问题，由于时间复杂度过大，移除了对错位数组的随机排序。

--- Cyjb/ArrayExt.cs
+++ Cyjb/ArrayExt.cs
@@ -382,11 +382,27 @@ namespace Cyjb
 		/// <typeparam name="T">数组中元素的类型。</typeparam>
 		/// <param name="array">要随机排序的数组。</param>
 		/// <returns>要随机排序数组。</returns>
+		/// <remarks>采用下面的代码进行测试：
+		/// <code>int size = 10;
+		/// int[] arr = new int[size];
+		/// int[,] cnt = new int[size, size];
+		/// for (int i = 0; i &lt; 200; i++)
+		/// {
+		/// 	arr.Fill(n => n).Random();
+		/// 	for (int j = 0; j &lt; size; j++) cnt[j, arr[j]]++;
+		/// }
+		/// for (int i = 0; i &lt; size; i++)
+		/// {
+		/// 	for (int j = 0; j &lt; size; j++)
+		/// 		Console.Write("{0} ", cnt[i, j]);
+		/// 	Console.WriteLine();
+		/// }</code>
+		/// </remarks>
 		public static T[] Random<T>(this T[] array)
 		{
 			for (int i = array.Length - 1; i > 0; i--)
 			{
-				int j = RandomExt.Next(i);
+				int j = RandomExt.Next(i + 1);
 				if (j != i)
 				{
 					T temp = array[i];
@@ -402,27 +418,42 @@ namespace Cyjb
 		/// <typeparam name="T">数组中元素的类型。</typeparam>
 		/// <param name="array">要随机排序的数组。</param>
 		/// <returns>要随机排序数组。</returns>
+		/// <remarks>采用下面的代码进行测试：
+		/// <code>int w = 4;
+		/// int h = 3;
+		/// int size = w * h;
+		/// int[,] arr = new int[h, w];
+		/// int[,] cnt = new int[size, size];
+		/// for (int i = 0; i &lt; 320; i++)
+		/// {
+		/// 	arr.Fill((y, x) => y * w + x).Random();
+		/// 	for (int j = 0; j &lt; size; j++) cnt[j, arr[j / w, j % w]]++;
+		/// }
+		/// for (int i = 0; i &lt; size; i++)
+		/// {
+		/// 	for (int j = 0; j &lt; size; j++) Console.Write("{0} ", cnt[i, j]);
+		/// 	Console.WriteLine();
+		/// }</code>
+		/// </remarks>
 		[SuppressMessage("Microsoft.Performance", "CA1814:PreferJaggedArraysOverMultidimensional", MessageId = "Return")]
 		[SuppressMessage("Microsoft.Performance", "CA1814:PreferJaggedArraysOverMultidimensional", MessageId = "0#")]
 		public static T[,] Random<T>(this T[,] array)
 		{
+			int w = array.GetLength(1);
+			int idx = array.Length;
 			for (int i = array.GetLength(0) - 1; i >= 0; i--)
 			{
-				for (int j = array.GetLength(1) - 1; j >= 0; j--)
+				for (int j = w - 1; j >= 0; j--)
 				{
-					int a = RandomExt.Next(i);
-					int b = 0;
-					if (a != i)
+					int r = RandomExt.Next(idx--);
+					int x = r % w;
+					int y = r / w;
+					if (y != i || x != j)
 					{
-						b = RandomExt.Next(array.GetLength(1));
+						T temp = array[i, j];
+						array[i, j] = array[y, x];
+						array[y, x] = temp;
 					}
-					else
-					{
-						b = RandomExt.Next(j);
-					}
-					T temp = array[i, j];
-					array[i, j] = array[a, b];
-					array[a, b] = temp;
 				}
 			}
 			return array;
@@ -433,110 +464,49 @@ namespace Cyjb
 		/// <typeparam name="T">数组中元素的类型。</typeparam>
 		/// <param name="array">要随机排序的数组。</param>
 		/// <returns>要随机排序数组。</returns>
-		[SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter"), CLSCompliant(false)]
-		public static T[][] Random<T>(this T[][] array)
-		{
-			for (int i = array.Length - 1; i >= 0; i--)
-			{
-				for (int j = array[i].Length - 1; j >= 0; j--)
-				{
-					int a = RandomExt.Next(i);
-					int b = 0;
-					if (a != i)
-					{
-						b = RandomExt.Next(array[a].Length);
-					}
-					else
-					{
-						b = RandomExt.Next(j);
-					}
-					T temp = array[i][j];
-					array[i][j] = array[a][b];
-					array[a][b] = temp;
-				}
-			}
-			return array;
-		}
-		/// <summary>
-		/// 将数组进行随机排序。
-		/// </summary>
-		/// <typeparam name="T">数组中元素的类型。</typeparam>
-		/// <param name="array">要随机排序的数组。</param>
-		/// <returns>要随机排序数组。</returns>
+		/// <remarks>采用下面的代码进行测试：
+		/// <code>int w = 2;
+		/// int h = 2;
+		/// int d = 3;
+		/// int size = w * h * d;
+		/// int[, ,] arr = new int[d, h, w];
+		/// int[,] cnt = new int[size, size];
+		/// for (int i = 0; i &lt; 240; i++)
+		/// {
+		/// 	arr.Fill((z, y, x) => z * w * h + y * w + x);
+		/// 	arr.Random();
+		/// 	for (int j = 0; j &lt; size; j++) cnt[j, arr[j / (w * h), j / w % h, j % w]]++;
+		/// }
+		/// for (int i = 0; i &lt; size; i++)
+		/// {
+		/// 	for (int j = 0; j &lt; size; j++) Console.Write("{0} ", cnt[i, j]);
+		/// 	Console.WriteLine();
+		/// }</code>
+		/// </remarks>
 		[SuppressMessage("Microsoft.Performance", "CA1814:PreferJaggedArraysOverMultidimensional", MessageId = "0#"),]
 		[SuppressMessage("Microsoft.Performance", "CA1814:PreferJaggedArraysOverMultidimensional", MessageId = "Return")]
 		public static T[, ,] Random<T>(this T[, ,] array)
 		{
+			int h = array.GetLength(1);
+			int w = array.GetLength(2);
+			int idx = array.Length;
 			for (int i = array.GetLength(0) - 1; i >= 0; i--)
 			{
-				for (int j = array.GetLength(1) - 1; j >= 0; j--)
+				for (int j = h - 1; j >= 0; j--)
 				{
-					for (int k = array.GetLength(2) - 1; k >= 0; k--)
+					for (int k = w - 1; k >= 0; k--)
 					{
-						int a = RandomExt.Next(i);
-						int b = 0, c = 0;
-						if (a != i)
+						int r = RandomExt.Next(idx--);
+						int x = r % w;
+						int z = r / w;
+						int y = z % h;
+						z /= h;
+						if (z != i || y != j || x != k)
 						{
-							b = RandomExt.Next(array.GetLength(1));
-							c = RandomExt.Next(array.GetLength(2));
+							T temp = array[i, j, k];
+							array[i, j, k] = array[z, y, x];
+							array[z, y, x] = temp;
 						}
-						else
-						{
-							b = RandomExt.Next(j);
-							if (b != j)
-							{
-								c = RandomExt.Next(array.GetLength(2));
-							}
-							else
-							{
-								c = RandomExt.Next(b);
-							}
-						}
-						T temp = array[i, j, k];
-						array[i, j, k] = array[a, b, c];
-						array[a, b, c] = temp;
-					}
-				}
-			}
-			return array;
-		}
-		/// <summary>
-		/// 将数组进行随机排序。
-		/// </summary>
-		/// <typeparam name="T">数组中元素的类型。</typeparam>
-		/// <param name="array">要随机排序的数组。</param>
-		/// <returns>要随机排序数组。</returns>
-		[SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter"), CLSCompliant(false)]
-		public static T[][][] Random<T>(this T[][][] array)
-		{
-			for (int i = array.Length - 1; i >= 0; i--)
-			{
-				for (int j = array[i].Length - 1; j >= 0; j--)
-				{
-					for (int k = array[i][j].Length - 1; k >= 0; k--)
-					{
-						int a = RandomExt.Next(i);
-						int b = 0, c = 0;
-						if (a != i)
-						{
-							b = RandomExt.Next(array[a].Length);
-							c = RandomExt.Next(array[a][b].Length);
-						}
-						else
-						{
-							b = RandomExt.Next(j);
-							if (b != j)
-							{
-								c = RandomExt.Next(array[a][b].Length);
-							}
-							else
-							{
-								c = RandomExt.Next(k);
-							}
-						}
-						T temp = array[i][j][k];
-						array[i][j][k] = array[a][b][c];
-						array[a][b][c] = temp;
 					}
 				}
 			}
