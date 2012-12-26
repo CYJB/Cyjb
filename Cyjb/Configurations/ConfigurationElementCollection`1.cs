3e47649b323ac9a3a29060d56913220e8868193f
CYJB <f8cyjb@126.com>
2012-12-26 22:50:34 +0800

    添加了配置元素集合的强类型基类。

---
+++ Cyjb/Configurations/ConfigurationElementCollection`1.cs
@@ -0,0 +1,314 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace Cyjb.Configurations
+{
+	/// <summary>
+	/// 表示强类型的包含一个子元素集合的配置元素。
+	/// </summary>
+	/// <typeparam name="TElement">子配置元素的类型。</typeparam>
+	public abstract class ConfigurationElementCollection<TElement> :
+		ConfigurationElementCollection, IList<TElement>, IList, ICollection<TElement>, ICollection
+		where TElement : ConfigurationElement
+	{
+
+		#region IList<TElement> 成员
+
+		/// <summary>
+		/// 获取或设置指定索引处的元素。
+		/// </summary>
+		/// <param name="index">要获取或设置的元素从零开始的索引。</param>
+		/// <value>指定索引处的元素。</value>
+		public TElement this[int index]
+		{
+			get { return base.BaseGet(index) as TElement; }
+			set
+			{
+				// 先添加后删除，如果添加失败，不会导致配置元素被删除。
+				this.BaseAdd(index, value);
+				base.BaseRemoveAt(index + 1);
+			}
+		}
+
+		/// <summary>
+		/// 确定 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中特定项的索引。
+		/// </summary>
+		/// <param name="item">要在 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 
+		/// 中定位的对象。</param>
+		/// <returns>如果在 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 
+		/// 中找到 <paramref name="item"/>，则为该项的索引；否则为 <c>-1</c>。</returns>
+		public int IndexOf(TElement item)
+		{
+			return base.BaseIndexOf(item);
+		}
+
+		/// <summary>
+		/// 在 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中的指定索引处插入项。
+		/// </summary>
+		/// <param name="index">从零开始的索引，应在该位置插入 <paramref name="item"/>。</param>
+		/// <param name="item">要插入到 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中的对象。</param>
+		/// <exception cref="System.ArgumentOutOfRangeException"><paramref name="index"/> 
+		/// 不是 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中的有效索引。</exception>
+		public void Insert(int index, TElement item)
+		{
+			this.BaseAdd(index, item);
+		}
+
+		/// <summary>
+		/// 移除指定索引处的 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 项。
+		/// </summary>
+		/// <param name="index">从零开始的索引（属于要移除的项）。</param>
+		/// <exception cref="System.ArgumentOutOfRangeException"><paramref name="index"/> 
+		/// 不是 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中的有效索引。</exception>
+		public void RemoveAt(int index)
+		{
+			base.BaseRemoveAt(index);
+		}
+
+		#endregion // IList<TElement> 成员
+
+		#region IList 成员
+
+		/// <summary>
+		/// 获取一个值，该值指示 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 是否具有固定大小。
+		/// </summary>
+		/// <value>如果 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 
+		/// 具有固定大小，则为 <c>true</c>；否则为 <c>false</c>。</value>
+		bool IList.IsFixedSize
+		{
+			get { return false; }
+		}
+
+		/// <summary>
+		/// 获取一个值，该值指示 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 是否为只读。
+		/// </summary>
+		/// <value>如果 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 
+		/// 为只读，则为 <c>true</c>；否则为 <c>false</c>。</value>
+		bool IList.IsReadOnly
+		{
+			get { return base.IsReadOnly(); }
+		}
+
+		/// <summary>
+		/// 获取或设置指定索引处的元素。
+		/// </summary>
+		/// <param name="index">要获得或设置的元素从零开始的索引。</param>
+		/// <value>指定索引处的元素。</value>
+		/// <exception cref="System.ArgumentOutOfRangeException"><paramref name="index"/> 
+		/// 不是 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中的有效索引。</exception>
+		object IList.this[int index]
+		{
+			get { return base.BaseGet(index); }
+			set
+			{
+				TElement item = value as TElement;
+				if (item != null)
+				{
+					// 先添加后删除，如果添加失败，不会导致配置元素被删除。
+					this.BaseAdd(index, item);
+					base.BaseRemoveAt(index + 1);
+				}
+				else
+				{
+					throw ExceptionHelper.ArgumentWrongType(value, typeof(TElement));
+				}
+			}
+		}
+
+		/// <summary>
+		/// 向 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中添加项。
+		/// </summary>
+		/// <param name="value">要添加到 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 的对象。</param>
+		/// <returns>新元素所插入到的位置，或为 <c>-1</c> 以指示未将该项插入到集合中。</returns>
+		int IList.Add(object value)
+		{
+			int idx = this.Count;
+			TElement item = value as TElement;
+			if (item == null)
+			{
+				throw ExceptionHelper.ArgumentWrongType(value, typeof(TElement));
+			}
+			else
+			{
+				this.BaseAdd(idx, item);
+			}
+			return idx;
+		}
+
+		/// <summary>
+		/// 从 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中移除所有项。
+		/// </summary>
+		void IList.Clear()
+		{
+			base.BaseClear();
+		}
+
+		/// <summary>
+		/// 确定 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 是否包含特定值。
+		/// </summary>
+		/// <param name="value">要在 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中定位的对象。</param>
+		/// <returns>如果在 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中找到 <paramref name="value"/>，则为 <c>true</c>；否则为 <c>false</c>。</returns>
+		bool IList.Contains(object value)
+		{
+			TElement item = value as TElement;
+			if (item != null)
+			{
+				return this.Contains(item);
+			}
+			return false;
+		}
+
+		/// <summary>
+		/// 确定 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中特定项的索引。
+		/// </summary>
+		/// <param name="value">要在 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中定位的对象。</param>
+		/// <returns>如果在 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中找到 <paramref name="value"/>，则为该项的索引；否则为 <c>-1</c>。</returns>
+		int IList.IndexOf(object value)
+		{
+			TElement item = value as TElement;
+			if (item != null)
+			{
+				return base.BaseIndexOf(item);
+			}
+			return -1;
+		}
+
+		/// <summary>
+		/// 在 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中的指定索引处插入项。
+		/// </summary>
+		/// <param name="index">应插入 <paramref name="value"/> 的位置的零始索引。</param>
+		/// <param name="value">要插入 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中的对象。</param>
+		/// <exception cref="System.ArgumentOutOfRangeException"><paramref name="index"/> 不是 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中的有效索引。</exception>
+		void IList.Insert(int index, object value)
+		{
+			TElement item = value as TElement;
+			if (item == null)
+			{
+				throw ExceptionHelper.ArgumentWrongType(value, typeof(TElement));
+			}
+			else
+			{
+				this.BaseAdd(index, item);
+			}
+		}
+
+		/// <summary>
+		/// 从 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中移除特定对象的第一个匹配项。
+		/// </summary>
+		/// <param name="value">要从 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中移除的对象。</param>
+		void IList.Remove(object value)
+		{
+			TElement item = value as TElement;
+			if (item != null)
+			{
+				this.Remove(item);
+			}
+		}
+
+		/// <summary>
+		/// 移除指定索引处的 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 项。
+		/// </summary>
+		/// <param name="index">要移除的项的从零开始的索引。</param>
+		/// <exception cref="System.ArgumentOutOfRangeException"><paramref name="index"/> 不是 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中的有效索引。</exception>
+		void IList.RemoveAt(int index)
+		{
+			this.RemoveAt(index);
+		}
+
+		#endregion // IList 成员
+
+		#region ICollection<TElement> 成员
+
+		/// <summary>
+		/// 获取一个值，该值指示 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 是否为只读。
+		/// </summary>
+		/// <value>如果 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 为只读，则为 <c>true</c>；否则为 <c>false</c>。</value>
+		bool ICollection<TElement>.IsReadOnly
+		{
+			get { return base.IsReadOnly(); }
+		}
+
+		/// <summary>
+		/// 将某项添加到 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中。
+		/// </summary>
+		/// <param name="item">要添加到 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 的对象。</param>
+		public void Add(TElement item)
+		{
+			this.BaseAdd(item);
+		}
+
+		/// <summary>
+		/// 从 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中移除所有项。
+		/// </summary>
+		public void Clear()
+		{
+			base.BaseClear();
+		}
+
+		/// <summary>
+		/// 确定 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 是否包含特定值。
+		/// </summary>
+		/// <param name="item">要在 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中定位的对象。</param>
+		public bool Contains(TElement item)
+		{
+			return base.BaseIndexOf(item) >= 0;
+		}
+
+		/// <summary>
+		/// 从特定的 <see cref="System.Array"/> 索引处开始，将 
+		/// <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 的元素复制到一个 
+		/// <see cref="System.Array"/> 中。
+		/// </summary>
+		/// <param name="array">作为从 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 
+		/// 复制的元素的目标位置的一维 <see cref="System.Array"/>。<see cref="System.Array"/> 
+		/// 必须具有从零开始的索引。</param>
+		/// <param name="arrayIndex"><paramref name="array"/> 中从零开始的索引，在此处开始复制。</param>
+		/// <exception cref="System.ArgumentNullException"><paramref name="array"/> 为 <c>null</c>。</exception>
+		/// <exception cref="System.ArgumentOutOfRangeException"><paramref name="arrayIndex"/> 小于零。</exception>
+		/// <exception cref="System.ArgumentException"><paramref name="array"/> 是多维的。</exception>
+		/// <exception cref="System.ArgumentException">源 
+		/// <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中的元素数目大于从 
+		/// <paramref name="arrayIndex"/> 到目标 <paramref name="array"/> 末尾之间的可用空间。</exception>
+		public void CopyTo(TElement[] array, int arrayIndex)
+		{
+			base.CopyTo(array, arrayIndex);
+		}
+
+		/// <summary>
+		/// 从 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中移除特定对象的第一个匹配项。
+		/// </summary>
+		/// <param name="item">要从 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中移除的对象。</param>
+		/// <returns>如果已从 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中成功移除 <paramref name="item"/>，则为 <c>true</c>；否则为 <c>false</c>。如果在原始 <see cref="ConfigurationElementCollection&lt;TElement&gt;"/> 中没有找到 <paramref name="item"/>，该方法也会返回 <c>false</c>。</returns>
+		public bool Remove(TElement item)
+		{
+			int idx = base.BaseIndexOf(item);
+			if (idx >= 0)
+			{
+				base.BaseRemoveAt(idx);
+				return true;
+			}
+			return false;
+		}
+
+		#endregion // ICollection<TElement> 成员
+
+		#region IEnumerable<TElement> 成员
+
+		/// <summary>
+		/// 返回一个循环访问集合的枚举器。
+		/// </summary>
+		/// <returns>可用于循环访问集合的 <see cref="System.Collections.Generic.IEnumerator&lt;T&gt;"/>。</returns>
+		public new IEnumerator<TElement> GetEnumerator()
+		{
+			IEnumerator enumerator = base.GetEnumerator();
+			while (enumerator.MoveNext())
+			{
+				yield return (TElement)enumerator.Current;
+			}
+		}
+
+		#endregion // IEnumerable<TElement> 成员
+
+	}
+}
---
+++ Cyjb/Configurations/ConfigurationElementCollection`2.cs
@@ -0,0 +1,54 @@
+﻿using System.Configuration;
+
+namespace Cyjb.Configurations
+{
+	/// <summary>
+	/// 表示强类型的包含一个子元素集合的配置元素。
+	/// </summary>
+	/// <typeparam name="TKey">子配置元素的键的类型。</typeparam>
+	/// <typeparam name="TElement">子配置元素的类型。</typeparam>
+	public abstract class ConfigurationElementCollection<TKey, TElement> : ConfigurationElementCollection<TElement>
+		where TElement : ConfigurationElement
+	{
+		/// <summary>
+		/// 基于所提供的键，从集合中移除 <see cref="System.Configuration.ConfigurationElement"/> 对象。
+		/// </summary>
+		/// <param name="key">要移除的 <see cref="System.Configuration.ConfigurationElement"/> 
+		/// 对象的键。</param>
+		public void Remove(TKey key)
+		{
+			base.BaseRemove(key);
+		}
+		/// <summary>
+		/// 基于所提供的参数，获取或设置子配置元素。
+		/// </summary>
+		/// <param name="key">集合中包含的子配置元素的键。</param>
+		/// <returns>子配置元素，如果不存在则返回 <c>null</c>。</returns>
+		public TElement this[TKey key]
+		{
+			get { return base.BaseGet(key as object) as TElement; }
+			set
+			{
+				object objKey = key;
+				ConfigurationElement item = base.BaseGet(objKey);
+				if (item != null)
+				{
+					base.BaseRemove(objKey);
+				}
+				try
+				{
+					this.BaseAdd(value);
+				}
+				finally
+				{
+					// 出错了，撤销删除。
+					if (item != null)
+					{
+						this.BaseAdd(item);
+					}
+				}
+			}
+		}
+
+	}
+}
