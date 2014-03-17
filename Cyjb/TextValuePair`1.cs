bfc8859142fbcdb14745429703ab161e8a58a3f3
CYJB <shaojun.yang@gmail.com>
2014-03-17 13:52:19 +0800

    更新了注释。

--- Cyjb/TextValuePair`1.cs
+++ Cyjb/TextValuePair`1.cs
@@ -315,7 +315,7 @@ namespace Cyjb
 		/// <summary>
 		/// 到 <see cref="TextValuePair"/> 类型的隐式类型转换。
 		/// </summary>
-		/// <param name="token">要转换类型的 <see cref="TextValuePair&lt;TValue&gt;"/> 实例。</param>
+		/// <param name="pair">要转换类型的 <see cref="TextValuePair&lt;TValue&gt;"/> 实例。</param>
 		/// <returns>类型转换的结果。</returns>
 		public static implicit operator TextValuePair(TextValuePair<TValue> pair)
 		{
@@ -324,7 +324,7 @@ namespace Cyjb
 		/// <summary>
 		/// 从 <see cref="TextValuePair"/> 类型的显式类型转换。
 		/// </summary>
-		/// <param name="token">要转换类型的 <see cref="TextValuePair"/> 实例。</param>
+		/// <param name="pair">要转换类型的 <see cref="TextValuePair"/> 实例。</param>
 		/// <returns>类型转换的结果。</returns>
 		public static explicit operator TextValuePair<TValue>(TextValuePair pair)
 		{
